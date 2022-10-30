78627eff9254a5a3c17ea91773eb6a1d7e4e9d4f
TalDmitrii <30574780+TalDmitrii@users.noreply.github.com>
2022-10-30 19:08:24 +0300

    Fix warnings

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Suspense, useEffect } from "react";
 import { Switch } from "react-router-dom";
 import { Route } from "react-router-dom";
-import { Redirect } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { productsActions } from "./store/index";
 
@@ -9,7 +8,6 @@ import classes from "./App.module.css";
 
 import Header from "./components/layout/Header/Header";
 import Footer from "./components/layout/Footer/Footer";
-import PageContainer from "./components/layout/PageContainer/PageContainer";
 import Loader from "./components/UI/Loader/Loader";
 
 const Catalog = React.lazy(() => import("./pages/Catalog"));
@@ -17,191 +15,188 @@ const MainPage = React.lazy(() => import("./pages/MainPage"));
 const NotFound = React.lazy(() => import("./pages/NotFound"));
 const ProductCard = React.lazy(() => import("./pages/ProductCard"));
 
-const bestsellers = [
-    {
-        id: 1,
-        title: "Jacket",
-        description: `Gorgeous jacket is the latest trend`,
-        price: 40,
-        colors: ["turquoise", "blue", "bluelight"],
-        sizes: ["xs", "s", "m", "l", "xl"],
-        gender: "female",
-        new: true,
-        bestseller: true,
-        paths: {
-            x1: "../bestsellers/1.jpg",
-            x2: "../bestsellers/1@2x.jpg",
-            previews: [
-                "../previews/1.jpg",
-                "../previews/2.jpg",
-                "../previews/3.jpg",
-                "../previews/4.jpg",
-            ],
-        },
-    },
-    {
-        id: 2,
-        title: "Overalls",
-        description: `Gorgeous Overalls is the latest trend`,
-        price: 1400,
-        colors: ["turquoise", "blue", "grey", "bluelight"],
-        sizes: ["xs", "s", "m", "l"],
-        gender: "female",
-        new: false,
-        bestseller: false,
-        paths: {
-            x1: "../bestsellers/2.jpg",
-            x2: "../bestsellers/2@2x.jpg",
-            previews: [
-                "../previews/1.jpg",
-                "../previews/2.jpg",
-                "../previews/3.jpg",
-                "../previews/4.jpg",
-            ],
-        },
-    },
-    {
-        id: 3,
-        title: "Jacket",
-        description: `Gorgeous Overalls is the latest trend`,
-        price: 910,
-        colors: ["turquoise", "blue", "grey"],
-        sizes: ["xs", "l", "xl"],
-        gender: "female",
-        new: false,
-        bestseller: true,
-        paths: {
-            x1: "../bestsellers/3.jpg",
-            x2: "../bestsellers/3@2x.jpg",
-            previews: [
-                "../previews/1.jpg",
-                "../previews/2.jpg",
-                "../previews/3.jpg",
-                "../previews/4.jpg",
-            ],
-        },
-    },
-    {
-        id: 4,
-        title: "Overalls",
-        description: `Gorgeous Overalls is the latest trend`,
-        price: 200,
-        colors: ["turquoise", "black", "bluelight"],
-        sizes: ["xs", "s", "m", "xl"],
-        gender: "female",
-        new: false,
-        bestseller: false,
-        paths: {
-            x1: "../bestsellers/4.jpg",
-            x2: "../bestsellers/4@2x.jpg",
-            previews: [
-                "../previews/1.jpg",
-                "../previews/2.jpg",
-                "../previews/3.jpg",
-                "../previews/4.jpg",
-            ],
-        },
-    },
-];
-
-const categories = [
-    {
-        id: 5,
-        title: "Jacket",
-        description: `Gorgeous Overalls is the latest trend`,
-        price: 360,
-        colors: ["grey", "black", "bluelight"],
-        sizes: ["m", "l", "xl"],
-        gender: "female",
-        new: true,
-        bestseller: true,
-        paths: {
-            x1: "../categories/1.jpg",
-            x2: "../categories/1@2x.jpg",
-            previews: [
-                "../previews/1.jpg",
-                "../previews/2.jpg",
-                "../previews/3.jpg",
-                "../previews/4.jpg",
-            ],
-        },
-    },
-    {
-        id: 6,
-        title: "Overalls",
-        description: `Gorgeous Overalls is the latest trend`,
-        price: 140,
-        colors: ["turquoise", "blue", "grey", "black"],
-        sizes: ["xs", "s", "xl"],
-        gender: "male",
-        new: true,
-        bestseller: false,
-        paths: {
-            x1: "../categories/2.jpg",
-            x2: "../categories/2@2x.jpg",
-            previews: [
-                "../previews/1.jpg",
-                "../previews/2.jpg",
-                "../previews/3.jpg",
-                "../previews/4.jpg",
-            ],
-        },
-    },
-    {
-        id: 7,
-        title: "Jacket",
-        description: `Gorgeous Jacket is the latest trend`,
-        price: 90,
-        colors: ["blue", "grey", "black"],
-        sizes: ["xs", "s", "m"],
-        gender: "male",
-        new: true,
-        bestseller: true,
-        paths: {
-            x1: "../categories/3.jpg",
-            x2: "../categories/3@2x.jpg",
-            previews: [
-                "../previews/1.jpg",
-                "../previews/2.jpg",
-                "../previews/3.jpg",
-                "../previews/4.jpg",
-            ],
-        },
-    },
-    {
-        id: 8,
-        title: "Overalls",
-        description: `Gorgeous Overalls is the latest trend`,
-        price: 160,
-        colors: ["turquoise", "blue", "grey", "black"],
-        sizes: ["xs", "m", "xl"],
-        gender: "male",
-        new: true,
-        bestseller: false,
-        paths: {
-            x1: "../categories/4.jpg",
-            x2: "../categories/4@2x.jpg",
-            previews: [
-                "../previews/1.jpg",
-                "../previews/2.jpg",
-                "../previews/3.jpg",
-                "../previews/4.jpg",
-            ],
-        },
-    },
-];
-
 function App() {
-    console.log("App render");
     const dispatch = useDispatch();
 
     useEffect(() => {
+        const bestsellers = [
+            {
+                id: 1,
+                title: "Jacket",
+                description: `Gorgeous jacket is the latest trend`,
+                price: 40,
+                colors: ["turquoise", "blue", "bluelight"],
+                sizes: ["xs", "s", "m", "l", "xl"],
+                gender: "female",
+                new: true,
+                bestseller: true,
+                paths: {
+                    x1: "../bestsellers/1.jpg",
+                    x2: "../bestsellers/1@2x.jpg",
+                    previews: [
+                        "../previews/1.jpg",
+                        "../previews/2.jpg",
+                        "../previews/3.jpg",
+                        "../previews/4.jpg",
+                    ],
+                },
+            },
+            {
+                id: 2,
+                title: "Overalls",
+                description: `Gorgeous Overalls is the latest trend`,
+                price: 1400,
+                colors: ["turquoise", "blue", "grey", "bluelight"],
+                sizes: ["xs", "s", "m", "l"],
+                gender: "female",
+                new: false,
+                bestseller: false,
+                paths: {
+                    x1: "../bestsellers/2.jpg",
+                    x2: "../bestsellers/2@2x.jpg",
+                    previews: [
+                        "../previews/1.jpg",
+                        "../previews/2.jpg",
+                        "../previews/3.jpg",
+                        "../previews/4.jpg",
+                    ],
+                },
+            },
+            {
+                id: 3,
+                title: "Jacket",
+                description: `Gorgeous Overalls is the latest trend`,
+                price: 910,
+                colors: ["turquoise", "blue", "grey"],
+                sizes: ["xs", "l", "xl"],
+                gender: "female",
+                new: false,
+                bestseller: true,
+                paths: {
+                    x1: "../bestsellers/3.jpg",
+                    x2: "../bestsellers/3@2x.jpg",
+                    previews: [
+                        "../previews/1.jpg",
+                        "../previews/2.jpg",
+                        "../previews/3.jpg",
+                        "../previews/4.jpg",
+                    ],
+                },
+            },
+            {
+                id: 4,
+                title: "Overalls",
+                description: `Gorgeous Overalls is the latest trend`,
+                price: 200,
+                colors: ["turquoise", "black", "bluelight"],
+                sizes: ["xs", "s", "m", "xl"],
+                gender: "female",
+                new: false,
+                bestseller: false,
+                paths: {
+                    x1: "../bestsellers/4.jpg",
+                    x2: "../bestsellers/4@2x.jpg",
+                    previews: [
+                        "../previews/1.jpg",
+                        "../previews/2.jpg",
+                        "../previews/3.jpg",
+                        "../previews/4.jpg",
+                    ],
+                },
+            },
+        ];
         dispatch(productsActions.refreshProducts([...bestsellers]));
-    }, [dispatch, bestsellers]);
+    }, [dispatch]);
 
     useEffect(() => {
+        const categories = [
+            {
+                id: 5,
+                title: "Jacket",
+                description: `Gorgeous Overalls is the latest trend`,
+                price: 360,
+                colors: ["grey", "black", "bluelight"],
+                sizes: ["m", "l", "xl"],
+                gender: "female",
+                new: true,
+                bestseller: true,
+                paths: {
+                    x1: "../categories/1.jpg",
+                    x2: "../categories/1@2x.jpg",
+                    previews: [
+                        "../previews/1.jpg",
+                        "../previews/2.jpg",
+                        "../previews/3.jpg",
+                        "../previews/4.jpg",
+                    ],
+                },
+            },
+            {
+                id: 6,
+                title: "Overalls",
+                description: `Gorgeous Overalls is the latest trend`,
+                price: 140,
+                colors: ["turquoise", "blue", "grey", "black"],
+                sizes: ["xs", "s", "xl"],
+                gender: "male",
+                new: true,
+                bestseller: false,
+                paths: {
+                    x1: "../categories/2.jpg",
+                    x2: "../categories/2@2x.jpg",
+                    previews: [
+                        "../previews/1.jpg",
+                        "../previews/2.jpg",
+                        "../previews/3.jpg",
+                        "../previews/4.jpg",
+                    ],
+                },
+            },
+            {
+                id: 7,
+                title: "Jacket",
+                description: `Gorgeous Jacket is the latest trend`,
+                price: 90,
+                colors: ["blue", "grey", "black"],
+                sizes: ["xs", "s", "m"],
+                gender: "male",
+                new: true,
+                bestseller: true,
+                paths: {
+                    x1: "../categories/3.jpg",
+                    x2: "../categories/3@2x.jpg",
+                    previews: [
+                        "../previews/1.jpg",
+                        "../previews/2.jpg",
+                        "../previews/3.jpg",
+                        "../previews/4.jpg",
+                    ],
+                },
+            },
+            {
+                id: 8,
+                title: "Overalls",
+                description: `Gorgeous Overalls is the latest trend`,
+                price: 160,
+                colors: ["turquoise", "blue", "grey", "black"],
+                sizes: ["xs", "m", "xl"],
+                gender: "male",
+                new: true,
+                bestseller: false,
+                paths: {
+                    x1: "../categories/4.jpg",
+                    x2: "../categories/4@2x.jpg",
+                    previews: [
+                        "../previews/1.jpg",
+                        "../previews/2.jpg",
+                        "../previews/3.jpg",
+                        "../previews/4.jpg",
+                    ],
+                },
+            },
+        ];
         dispatch(productsActions.refreshProducts([...categories]));
-    }, [dispatch, categories]);
+    }, [dispatch]);
 
     return (
         <div className={classes.app}>
