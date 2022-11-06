b46e3f32da57c6c8fa5ef93c1a8436b32a6f138c
TalDmitrii <30574780+TalDmitrii@users.noreply.github.com>
2022-11-06 18:36:39 +0300

    Load categories on intersection

--- src/components/CategoriesList/CategoriesList.js
+++ src/components/CategoriesList/CategoriesList.js
@@ -1,4 +1,7 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+
+import useIntersection from "../../hooks/use-intersection";
 
 import classes from "./CategoriesList.module.css";
 
@@ -10,8 +13,22 @@ const categories = [
 ];
 
 const CategoriesList = () => {
+    const { checkIntersection, isIntersected } = useIntersection();
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        isIntersected && setIsLoaded(true);
+    }, [isIntersected, setIsLoaded]);
+
+    useEffect(() => {
+        const intersectionTarget = document.querySelector(".j-categories");
+        if (!intersectionTarget) return;
+
+        checkIntersection({ target: intersectionTarget });
+    }, [checkIntersection]);
+
     return (
-        <section>
+        <section className="j-categories">
             <div className={classes["products__header-wrap"]}>
                 <h2 className={classes["products__section-header"]}>
                     Categories
@@ -23,30 +40,32 @@ const CategoriesList = () => {
                     View all
                 </Link>
             </div>
-            <ul className={classes["products__list"]}>
-                {categories.map((item) => (
-                    <li className={classes["products__item"]} key={item.id}>
-                        <Link
-                            to={`/catalog/categories/${item.title.toLocaleLowerCase()}`}
-                        />
-                        <picture>
-                            <source
-                                media="(min-width: 768px)"
-                                srcSet={`../categories/${item.title.toLocaleLowerCase()}@2x.jpg`}
+            {(isIntersected || isLoaded) && (
+                <ul className={classes["products__list"]}>
+                    {categories.map((item) => (
+                        <li className={classes["products__item"]} key={item.id}>
+                            <Link
+                                to={`/catalog/categories/${item.title.toLocaleLowerCase()}`}
                             />
-                            <img
-                                src={`../categories/${item.title.toLocaleLowerCase()}@1x.jpg`}
-                                alt={item.title}
-                                width="130"
-                                height="160"
-                            />
-                        </picture>
-                        <h3 className={classes["products__title"]}>
-                            {item.title}
-                        </h3>
-                    </li>
-                ))}
-            </ul>
+                            <picture>
+                                <source
+                                    media="(min-width: 768px)"
+                                    srcSet={`../categories/${item.title.toLocaleLowerCase()}@2x.jpg`}
+                                />
+                                <img
+                                    src={`../categories/${item.title.toLocaleLowerCase()}@1x.jpg`}
+                                    alt={item.title}
+                                    width="130"
+                                    height="160"
+                                />
+                            </picture>
+                            <h3 className={classes["products__title"]}>
+                                {item.title}
+                            </h3>
+                        </li>
+                    ))}
+                </ul>
+            )}
         </section>
     );
 };
